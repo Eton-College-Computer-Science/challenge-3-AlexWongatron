a4fb47632a1c8ba414406528deca571815979326
Alexander Wong <wong.a@etoncollege.org.uk>
2024-09-24 10:33:39 +0100

    checkpoint

--- Challenge 3 - Cities/main.swift
+++ Challenge 3 - Cities/main.swift
@@ -7,5 +7,35 @@
 
 import Foundation
 
-print("Hello, World!")
+var firstCity = ""
+var secondCity = ""
+var validFirstCity = false
+var validSecondCity = false
 
+func createExpression(firstCity: String, secondCity: String) -> String {
+    let cappedFirstCity = firstCity.uppercased()
+    let cappedSecondCity = secondCity.uppercased()
+    return "\(cappedFirstCity.dropFirst(4))-\(cappedSecondCity.dropFirst(4))"
+}
+
+print("Please input the first city of your choice")
+while !validFirstCity {
+    if let firstCity = readLine() {
+        validFirstCity = true
+    }
+    else {
+        print("You entered an invalid city! Try again!")
+    }
+}
+
+print("Please input the second city of your choice")
+while !validSecondCity {
+    if let secondCity = readLine() {
+        validSecondCity = true
+    }
+    else {
+        print("You entered an invalid city! Try again!")
+    }
+}
+
+if valid
