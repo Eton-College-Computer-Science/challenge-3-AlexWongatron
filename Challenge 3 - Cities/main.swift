87266485a53d16456c325509f67539c8adb32c02
Alexander Wong <wong.a@etoncollege.org.uk>
2024-09-24 10:59:37 +0100

    Completed

--- Challenge 3 - Cities/main.swift
+++ Challenge 3 - Cities/main.swift
@@ -12,15 +12,17 @@ var secondCity = ""
 var validFirstCity = false
 var validSecondCity = false
 
-func createExpression(firstCity: String, secondCity: String) -> String {
+func createExpression(firstCities: String, secondCities: String) -> String {
     let cappedFirstCity = firstCity.uppercased()
     let cappedSecondCity = secondCity.uppercased()
-    return "\(cappedFirstCity.dropFirst(4))-\(cappedSecondCity.dropFirst(4))"
+    return "\(cappedFirstCity.dropLast(2))-\(cappedSecondCity.dropLast(2))"
 }
 
 print("Please input the first city of your choice")
+
 while !validFirstCity {
-    if let firstCity = readLine() {
+    if let unwrappedInputOne = readLine() {
+        firstCity = unwrappedInputOne
         validFirstCity = true
     }
     else {
@@ -28,9 +30,11 @@ while !validFirstCity {
     }
 }
 
+
 print("Please input the second city of your choice")
 while !validSecondCity {
-    if let secondCity = readLine() {
+    if let unwrappedInputTwo = readLine() {
+        secondCity = unwrappedInputTwo
         validSecondCity = true
     }
     else {
@@ -38,4 +42,9 @@ while !validSecondCity {
     }
 }
 
-if valid
+if validFirstCity == true && validSecondCity == true {
+    let generatedStatement = createExpression(firstCities: firstCity, secondCities: secondCity)
+    print(generatedStatement)
+}
+
+
